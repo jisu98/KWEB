a1838a7e01349bf1471b1423dac5f055ebc5efcc
jisu98 <imsrhs60l@korea.ac.kr>
2018-11-14 20:44:26 +0900

    4주차 과제

---
+++ week2/index.js
@@ -0,0 +1,6 @@
+(param1, param2, ..., paramN) => { statements }
+(param1, param2, ..., paramN) => expression
+
+(singleParam) => { statements} === singleParam => { satements }
+
+() => { statements }
